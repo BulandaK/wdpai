7a19cf1ad4ad0202e44a8ee53f19a78c31fb9be9
Kamil Bulanda <eskimosl@wp.pl>
2025-01-03 16:13:44 +0100

    create main page

--- Public/views/main.php
+++ Public/views/main.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>strona guwna</title>
-  </head>
-  <body>
-    <h1>Strona guwna</h1>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../Public/css/main.css" />
+    <title>Cinema</title>
+</head>
+<body>
+    <header>
+        <h1>Cinema</h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Movies</a>
+            <a href="/login">Sign In</a>
+            <a href="/signUp">Sign Up</a>
+        </nav>
+    </header>
+    <div class="banner">
+    <img src="../Public/img/baners/film2-baner.jpeg" alt="" />
+    </div>
+    <section class="movies-section">
+        <h2>Upcoming Movies</h2>
+        <div class="movie-list">
+            <div class="movie-card">
+                <img src="../Public/img/cards/film-card1.jpeg" alt="Movie 1">
+                <div class="movie-info">
+                    <h3>Movie Title 1</h3>
+                    <p>Release Date: 2023-10-15</p>
+                </div>
+            </div>
+            <div class="movie-card">
+                <img src="../Public/img/cards/film-card2.jpeg" alt="Movie 2">
+                <div class="movie-info">
+                    <h3>Movie Title 2</h3>
+                    <p>Release Date: 2023-11-01</p>
+                </div>
+            </div>
+            <div class="movie-card">
+                <img src="../Public/img/cards/film-card3.jpeg" alt="Movie 3">
+                <div class="movie-info">
+                    <h3>Movie Title 3</h3>
+                    <p>Release Date: 2023-11-20</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
 </html>
